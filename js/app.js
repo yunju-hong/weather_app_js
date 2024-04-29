c52c7602e2e9be90b14b99d720fab66b2b57117a
yunju-hong <pulhae@gmail.com>
2024-04-29 12:50:01 +0900

    0429

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param {NodeList} elmts selected elements from HTML to add events
+ * @param {string} event event type e.g. 'click', 'mouseenter'...
+ * @param {Function} callback function to be executed when event is triggered
+ */
+export function eventOnElmts(elmts, event, callback) {
+  for (const elmt of elmts) {
+    elmt.addEventListener(event, callback);
+  }
+}
